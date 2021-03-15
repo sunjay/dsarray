a3e74525406d1544dfdf9ae503a9243ad295f510
Sunjay Varma <varma.sunjay@gmail.com>
2021-03-14 21:45:01 -0700

    Testing properties of empty array

--- tests/test_dsarray.c
+++ tests/test_dsarray.c
@@ -7,10 +7,12 @@ void setUp(void) {}
 // clean stuff up here
 void tearDown(void) {}
 
-void test_testing_framework(void) {
+void test_empty_array(void) {
     dsarray arr = dsarray_new(sizeof(int));
 
     TEST_ASSERT_EQUAL_UINT(dsarray_len(&arr), 0);
+    // Should not allocate when initially empty
+    TEST_ASSERT_EQUAL_UINT(dsarray_capacity(&arr), 0);
     TEST_ASSERT_TRUE(dsarray_is_empty(&arr));
 
     dsarray_destroy(&arr);
@@ -19,7 +21,7 @@ void test_testing_framework(void) {
 int main(void) {
     UNITY_BEGIN();
 
-    RUN_TEST(test_testing_framework);
+    RUN_TEST(test_empty_array);
 
     return UNITY_END();
 }
